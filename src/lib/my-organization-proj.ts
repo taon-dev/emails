a6e09769a223ef73454f109526399ba496f73625
Dariusz Filipiak <darekf77@gmail.com>
2026-09-13 16:44:07 +0200

    chore: update

--- src/lib/my-organization-proj.ts
+++ src/lib/my-organization-proj.ts
@@ -1,8 +1,4 @@
-import { Translation } from "@taon-dev/i18n/src";
-import { Taon } from "taon/src";
-
-const t = Translation.for(Taon.__FILE_RELATIVE_PATH, Taon.LANG_IMPORT_MAP);
 
 export function myOrgProj() {
-  t.gettext('My Translated Company Name!')
+  console.log('My Translated Company Name!')
 }
